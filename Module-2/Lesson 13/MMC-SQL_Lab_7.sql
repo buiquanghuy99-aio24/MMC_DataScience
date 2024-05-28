75201039c9c529bda46e92944c62279b03965ffa
Quang Huy <buiquanghuyk41@gmail.com>
2024-05-28 23:46:26 +0700

    Quang Huy nop bai tap buoi 14

--- Module-2/Lesson 13/MMC-SQL_Lab_7.sql
+++ Module-2/Lesson 13/MMC-SQL_Lab_7.sql
@@ -54,7 +54,6 @@ END $$
 DELIMITER ;
 
 -- Question 4. Cấu hình một Exam có nhiều nhất 5 Question
-
 DROP    TRIGGER IF EXISTS trg_BeforeInsertExamQuestion;
 DELIMITER $$
 CREATE  TRIGGER           trg_BeforeInsertExamQuestion
@@ -71,6 +70,127 @@ BEGIN
     IF question_count > 5 THEN
         SIGNAL SQLSTATE '45004'
         SET MESSAGE_TEXT = 'Maximum questions per exam exceeded. Limit: 5';
+    END IF;
+END $$
+DELIMITER ;
+
+-- Question 5. Tạo Trigger không cho User xóa tài khoản có Email là admin@gmail.com (các tài khoản khác có thể được xóa và sẽ xóa tất cả các thông tin liên quan tới User đó)
+DROP    TRIGGER IF EXISTS   trg_PreventAdminDelete;
+DELIMITER $$
+CREATE  TRIGGER             trg_PreventAdminDelete
+BEFORE DELETE ON `Account`
+FOR EACH ROW
+BEGIN
+    IF OLD.Email = 'admin@gmail.com' THEN
+        SIGNAL SQLSTATE '45005'
+        SET MESSAGE_TEXT = 'Cannot delete admin account';
+    END IF;
+END $$
+DELIMITER ;
+
+-- Question 6. Không dùng cấu hình Default cho Field DepartmentID của bảng Account, hãy tạo Trigger cho phép User khi tạo Account không điền vào DepartmentID thì sẽ được phân vào phòng ban "Phòng chờ"
+DROP    TRIGGER IF EXISTS   trg_DefaultDepartmentOnInsert;
+DELIMITER $$
+CREATE  TRIGGER             trg_DefaultDepartmentOnInsert
+BEFORE INSERT ON `Account`
+FOR EACH ROW
+BEGIN
+    DECLARE default_deptID INT;
+
+    INSERT INTO `Department` (DepartmentName)
+    SELECT      'Phòng chờ'
+    WHERE       NOT EXISTS (SELECT  1
+                            FROM    `Department`
+                            WHERE   DepartmentName = 'Phòng chờ'
+                            );
+
+    SELECT  DepartmentID INTO default_deptID
+    FROM    `Department`
+    WHERE   DepartmentName = 'Phòng chờ'
+    LIMIT   1;
+
+    IF NEW.DepartmentID IS NULL THEN
+        SET NEW.DepartmentID = default_deptID;
+    END IF;
+END $$
+DELIMITER ;
+
+-- Question 7. Cấu hình 1 Bài thi chỉ cho phép User tạo tối đa 4 Answers cho mỗi Question, trong đó có tối đa 2 đáp án đúng
+DROP    TRIGGER IF EXISTS   trg_ValidateAnswers;
+DELIMITER $$
+CREATE  TRIGGER             trg_ValidateAnswers
+BEFORE INSERT ON `ExamQuestion`
+FOR EACH ROW
+BEGIN
+    DECLARE answer_count            INT;
+    DECLARE correct_answer_count    INT;
+
+    SELECT  COUNT(a.AnswerID)   , SUM(a.isCorrect = TRUE)
+    INTO    answer_count        , correct_answer_count
+    FROM    `Answer` AS a
+    WHERE   a.QuestionID = NEW.QuestionID;
+
+    IF answer_count >= 4 THEN
+        SIGNAL SQLSTATE '45007'
+        SET MESSAGE_TEXT = 'Each question can have a maximum of 4 answers.';
+    END IF;
+
+    IF correct_answer_count >= 2 THEN
+        SIGNAL SQLSTATE '45007'
+        SET MESSAGE_TEXT = 'Each question can have a maximum of 2 correct answers.';
+    END IF;
+END $$
+DELIMITER ;
+
+-- Question 9: Viết Trigger không cho phép User xóa Bài thi mới tạo được 2 ngày
+DROP    TRIGGER IF EXISTS   trg_PreventDeleteRecentExams;
+DELIMITER $$
+CREATE  TRIGGER             trg_PreventDeleteRecentExams
+BEFORE DELETE ON `Exam`
+FOR EACH ROW
+BEGIN
+    IF (OLD.CreateDate >= NOW() - INTERVAL 2 DAY) THEN
+        SIGNAL SQLSTATE '45008'
+        SET MESSAGE_TEXT = 'Cannot  delete exams within the last 2 days';
+    END IF;
+END $$
+DELIMITER ;
+
+-- Question 10. Viết Trigger cho phép User chỉ được Update, Delete các Question khi nó chưa nằm trong Exam nào
+DROP    TRIGGER IF EXISTS   trg_RestrictUpdateOnQuestion;
+DELIMITER $$
+CREATE  TRIGGER             trg_RestrictUpdateOnQuestion
+BEFORE UPDATE ON `Question`
+FOR EACH ROW
+BEGIN
+    DECLARE exam_count INT;
+
+    SELECT  COUNT(*) INTO exam_count
+    FROM    `ExamQuestion`
+    WHERE   QuestionID = OLD.QuestionID;
+    
+    IF exam_count > 0 THEN
+        SIGNAL SQLSTATE '45010'
+        SET MESSAGE_TEXT = 'Update not allowed: The question is part of an exam.';
+    END IF;
+END $$
+DELIMITER ;
+
+DROP    TRIGGER IF EXISTS   trg_RestrictDeleteOnQuestion;
+DELIMITER $$
+CREATE  TRIGGER             trg_RestrictDeleteOnQuestion
+BEFORE DELETE ON `Question`
+FOR EACH ROW
+BEGIN
+    DECLARE exam_count INT;
+
+    SELECT  COUNT(*) INTO exam_count
+    FROM    `ExamQuestion`
+    WHERE   QuestionID = OLD.QuestionID;
+    
+    IF exam_count > 0 THEN
+        SIGNAL SQLSTATE '45010'
+        SET MESSAGE_TEXT = 'Delete not allowed: The question is part of an exam.';
     END IF;
 END $$
 DELIMITER ;
